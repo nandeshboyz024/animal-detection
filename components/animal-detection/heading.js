088c391d43ac97dcbbdca8241cb591d19f4ed7cc
nandeshboyz <nandeshboyz024@gmail.com>
2024-01-04 21:46:41 +0530

    done

--- components/animal-detection/heading.js
+++ components/animal-detection/heading.js
@@ -5,7 +5,7 @@ export default function Heading() {
   return (
     <div>
         <div style={{background:'linear-gradient(to right, #5170FF, #FF66C4)', display:'flex'}}>
-          <Image src="/imgs/VizLogo.gif" style={{margin:'10px'}} width={50} height={50} alt="VizLogo" />
+          <Image src="/imgs/viz.gif" style={{margin:'10px'}} width={50} height={50} alt="Viz-Logo" />
           <h2 style={{color:'white', marginTop:'20px'}}>Vizuara</h2>
           <Link href="/animal-detection/home" style={{marginLeft: 'auto', color:'white', marginTop:'30px',marginRight:'20px'}}>Homepage</Link>
         </div>
