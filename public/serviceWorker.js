90cd396e8e70f3e162d981c7bd5ee7b5bbf5da3f
nandeshboyz <nandeshboyz024@gmail.com>
2024-01-04 22:24:14 +0530

    try this

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -13,7 +13,8 @@ export function register(config) {
       window.location.hostname === '[::1]' ||
       window.location.hostname.match(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
+      )||
+      window.location.hostname==='animal-detection-eta.vercel.app'
     );
 
     const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
