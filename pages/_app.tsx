f6b0a6fa53cacf051fdb430b6de97e2d012f489b
nandeshboyz <nandeshboyz024@gmail.com>
2024-01-06 18:52:53 +0530

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import * as servicWorker from './serviceWorker';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps}/>
 }
+servicWorker.unregister();
 
